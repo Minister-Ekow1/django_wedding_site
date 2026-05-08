cd364a4259087e645240d2024d967cdc84bb282b
Minister-Ekow1 <ekowessandoh8492@gmail.com>
2026-05-08 15:11:33 +0000

    Fixed Render superuser creation done

--- build.sh
+++ build.sh
@@ -7,15 +7,3 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 python manage.py migrate
-
-python manage.py shell -c "
-from django.contrib.auth import get_user_model;
-User = get_user_model();
-
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser(
-        'admin',
-        'ekowessandoh8492@gmail.com',
-        '@Paakow1234'
-    )
-"
