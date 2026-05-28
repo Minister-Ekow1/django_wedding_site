d274e90a977563353bf846852191f965beec77c2
Minister-Ekow <ekowessandoh8492@gmail.comgit config --global user.email ekowessandoh8492@gmail.com>
2026-05-28 15:30:34 +0000

    back to default

--- build.sh
+++ build.sh
@@ -7,8 +7,3 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 python manage.py migrate
-
-python manage.py createsuperuser \
---noinput \
---username weddingadmin \
---email your@email.com || true
